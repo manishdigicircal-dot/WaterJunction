b235ff1ff2e470b4c018ef8c5b4f8e85043493d4
Manish <manish.digicircal@gmail.com>
2025-12-15 15:20:15 +0530

    fix: cors for vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,23 @@ app.set('trust proxy', 1);
 // Middleware
 app.use(helmet());
 app.use(morgan('dev'));
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//   credentials: true
+// }));mn
+
+
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, curl)
+    if (!origin) return callback(null, true);
+
+    return callback(null, true); // allow all trusted origins
+  },
   credentials: true
 }));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
