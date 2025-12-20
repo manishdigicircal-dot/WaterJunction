23fcd12e781c21abef965f282fe5123c34579009
Manish <manish.digicircal@gmail.com>
2025-12-20 11:38:19 +0530

    Fix CORS: Add Cache-Control to allowed headers

--- backend/server.js
+++ backend/server.js
@@ -107,7 +107,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control'],
   exposedHeaders: ['Content-Range', 'X-Content-Range'],
   maxAge: 86400 // 24 hours
 }));
